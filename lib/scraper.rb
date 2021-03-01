24b7005fc11eb88673deef7367d4f63fa18227d1
ammar-64 <ammar.n.sammour@gmail.com>
2021-03-01 18:34:21 +0300

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,8 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+headers = doc.css("h2")
+headers.each do |header|  
+    puts header.text.strip
+end
